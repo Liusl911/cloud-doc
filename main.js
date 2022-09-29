2e4a3d975265aa74ffe36b0469852ce2afb3136b
Liusl <allyllys@163.com>
2022-09-29 17:49:59 +0800

    coding

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron')
 const AppWindow = require('./src/AppWindow')
-const remote = require("@electron/remote/main") // remote 引用
-remote.initialize()
 const isDev = require('electron-is-dev')
+const { autoUpdater } = require("electron-updater")
+const remote = require("@electron/remote/main") // remote 引用
+remote.initialize() // 注册remote
 const Store = require('electron-store')
-Store.initRenderer();
+Store.initRenderer() // 注册electron-store
 const settingsStore = new Store({ name: 'Settings' })
 const fileStore = new Store({ name: 'Files Data' })
 const menuTemplate = require('./src/menuTemplate')
@@ -23,6 +24,50 @@ const createManager = () => {
 }
 
 app.on('ready', () => {
+
+    if (isDev) {
+        autoUpdater.updateConfigPath = path.join(__dirname, 'dev-app-update.yml')
+    }
+    autoUpdater.autoDownload = false
+    autoUpdater.checkForUpdatesAndNotify()
+    autoUpdater.on('error', (error) => {
+        dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
+    })
+    autoUpdater.on('checking-for-update', () => {
+        console.log('Checking for update...');
+    })
+    autoUpdater.on('update-available', () => {
+        dialog.showMessageBox({
+            type: 'info',
+            title: '应用有新的版本',
+            message: '发现新版本，是否现在更新?',
+            buttons: ['是', '否']
+        }, (buttonIndex) => {
+            if (buttonIndex === 0) {
+                autoUpdater.downloadUpdate()
+            }
+        })
+    })
+    autoUpdater.on('update-not-available', () => {
+        dialog.showMessageBox({
+            title: '没有新版本',
+            message: '当前已经是最新版本'
+        })
+    })
+    autoUpdater.on('download-progress', (progressObj) => {
+        let log_message = "Download speed: " + progressObj.bytesPerSecond;
+        log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+        console.log(log_message)
+    })
+    autoUpdater.on('update-downloaded', () => {
+        dialog.showMessageBox({
+            title: '安装更新',
+            message: '更新下载完毕，应用将重启并进行安装'
+        }, () => {
+            setImmediate(() => autoUpdater.quitAndInstall())
+        })
+    })
 
     const mainWindowConfig = {
         width: 1080,
